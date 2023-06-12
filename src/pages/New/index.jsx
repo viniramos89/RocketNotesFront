beba0ba8e43c0ecba5e6db3e0cc91fc98d840b8b
Vinicius Ramos <viniramos89@gmail.com>
2023-06-12 17:13:04 -0300

    Add files via upload

---
+++ src/pages/New/index.jsx
@@ -0,0 +1,148 @@
+import { useNavigate } from 'react-router-dom';
+import { Header } from '../../components/Header';
+import { Input } from '../../components/Input';
+import { Textarea } from '../../components/Textarea';
+import { NoteItem } from '../../components/NoteItem';
+import { Button } from '../../components/Button';
+import { Section } from '../../components/Section'
+import { useState } from 'react';
+import { api } from '../../services/api';
+import { ButtonText } from '../../components/ButtonText';
+
+import { Container, Form } from './styles';
+
+export function New(){
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const [links, setLinks] = useState([]);
+  const [newLink, setNewLink] = useState("");
+
+  const [tags, setTags] = useState([]);
+  const [newTag, setNewTag] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleBack(){
+    navigate(-1)
+  }
+
+  function handleAddLink(){
+    setLinks(prevState => [...prevState, newLink]);
+    setNewLink("");
+  }
+
+  function handleRemoveLink(linkDeleted){
+    setLinks(prevState => prevState.filter(link => link !== linkDeleted));
+  }
+
+  function handleAddTag(){
+    setTags(prevState => [...prevState, newTag]);
+    setNewTag("");
+  }
+
+  function handleRemoveTag(tagDeleted) {
+    setTags(prevState => prevState.filter(tag => tag !== tagDeleted))
+  }
+
+  async function handleNewNote(){
+    if(!title){
+      return alert("Adicione um título para a nota")
+    }
+    
+    if(newLink){
+      return alert("Você deixou de salvar um link")
+    }
+    
+    if(newTag){
+      return alert("Você deixou de salvar uma tag ");
+    }
+
+    await api.post("/notes", {
+      title,
+      description, 
+      tags,
+      links
+    });
+
+    alert("Nota criada com sucesso!");
+    navigate(-1);
+  }
+
+  return(
+    <Container>
+      <Header />
+
+      <main>
+        <Form>
+          <header>
+            <h1>Criar nota</h1>
+            <ButtonText 
+              title="Voltar"
+              onClick={handleBack}
+            />
+          </header>
+
+          <Input 
+            placeholder="Título"
+            onChange={e => setTitle(e.target.value)} 
+          />
+          <Textarea 
+            placeholder="Observações"
+            onChange={e => setDescription(e.target.value)}
+          />
+
+          <Section title="Links úteis">
+          {
+              links.map((link, index) => (
+                <NoteItem 
+                key={String(index)}
+                value={link}
+                onClick={() => handleRemoveLink(link)}
+              />
+            ))
+          }
+            <NoteItem 
+              isNew 
+              placeholder="Novo link"
+              value={newLink}
+              onChange={e => setNewLink(e.target.value)}
+              onClick={handleAddLink}
+            />
+          </Section>
+
+          <Section title="Marcadores">
+            <div className="tags">
+              {
+                tags.map((tag, index) => (
+                  <NoteItem 
+                    key={String(index)}
+                    value={tag}
+                    onClick={() => handleRemoveTag(tag)}
+                  /> 
+                ))
+
+              }
+
+              <NoteItem 
+                isNew 
+                placeholder="Nova tag"
+                onChange={e => setNewTag(e.target.value)}
+                value={newTag}
+                onClick={handleAddTag}
+              />
+            </div>
+          </Section>
+
+          <Button 
+            title="Salvar"
+            onClick={handleNewNote} 
+          />
+
+        </Form>
+      </main>
+    </Container>
+  );
+
+}
